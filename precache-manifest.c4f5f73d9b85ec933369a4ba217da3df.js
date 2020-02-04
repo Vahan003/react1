5dec028361db1511ad666e304c31a8dfba799bfc
Your Name <you@example.com>
2020-02-05 01:36:30 +0400

    Updates

---
+++ precache-manifest.c4f5f73d9b85ec933369a4ba217da3df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e8635ba5c924c57352d8f040bcc359",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "f416367429d718636dc6",
+    "url": "/react1/static/css/main.7d83f202.chunk.css"
+  },
+  {
+    "revision": "b17c2c24c6a145d05100",
+    "url": "/react1/static/js/2.9589a24e.chunk.js"
+  },
+  {
+    "revision": "f416367429d718636dc6",
+    "url": "/react1/static/js/main.d4c7f535.chunk.js"
+  },
+  {
+    "revision": "1d64972a69c9efa64035",
+    "url": "/react1/static/js/runtime-main.8fdba4e6.js"
+  }
+]);
